e7f5c9d28b15aa4a969eb0ed1caeb201338f949b
Sohaib Ibenhajene <91062358+SohaibIbenhajene@users.noreply.github.com>
2025-02-03 02:44:33 +0100

    Tags endpoints completed

---
+++ src/main/java/dev/sohaib/blog/controllers/TagController.java
@@ -0,0 +1,46 @@
+package dev.sohaib.blog.controllers;
+
+import dev.sohaib.blog.domain.dtos.CreateTagsRequest;
+import dev.sohaib.blog.domain.dtos.TagResponse;
+import dev.sohaib.blog.domain.entities.Tag;
+import dev.sohaib.blog.mappers.TagMapper;
+import dev.sohaib.blog.services.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/v1/tags")
+@RequiredArgsConstructor
+public class TagController {
+
+    private final TagService tagService;
+    private final TagMapper tagMapper;
+
+    @GetMapping
+    public ResponseEntity<List<TagResponse>> getAllTags() {
+        List<Tag> tags = tagService.getTags();
+        List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
+        return ResponseEntity.ok(tagResponses);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagsRequest createTagsRequest) {
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagResponses = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(
+                createdTagResponses,
+                HttpStatus.CREATED
+        );
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable UUID id) {
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
+    }
+}
